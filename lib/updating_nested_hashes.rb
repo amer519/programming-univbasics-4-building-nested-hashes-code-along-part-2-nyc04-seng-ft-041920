b949aff9a868ad3d0a4e9db7c721cd9e53dc97fa
Amer Fahmy <afahmy519@gmail.com>
2020-04-29 15:23:16 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -20,8 +20,10 @@ def update_status
       }
    }
 
-  romeo_dead = :hero => {status: "dead"}
-  juliet_dead = :heroine => {status: "dead"}
+         epic_tragedy[:montague][:hero][:status] = "dead"
+         epic_tragedy[:capulet][:heroine][:status] = "dead"
+         
+  
 
 
 
